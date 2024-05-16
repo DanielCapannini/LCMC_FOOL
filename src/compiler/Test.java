2b437b1265ec3f88ce997ae0bc554ffb0ed4a80a
DanielCapannini <daniel.capannini@studio.unibo.it>
2024-05-16 15:24:02 +0200

    Update Test.java

--- src/compiler/Test.java
+++ src/compiler/Test.java
@@ -10,7 +10,7 @@ import svm.*;
 public class Test {
     public static void main(String[] args) throws Exception {
    			
-    	String fileName = "res/test/oo/bankloan.fool";
+    	String fileName = "res/test/operators/minus/5minus3.fool";
 
     	CharStream chars = CharStreams.fromFileName(fileName);
     	FOOLLexer lexer = new FOOLLexer(chars);
